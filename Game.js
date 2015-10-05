587ccc76959201d3738991b26ac8ab87e09f5307
edwin0258 <edwin0258@gmail.com>
2015-10-04 18:39:51 -0700

    Transfer console.log statements to view in text area

--- Game.js
+++ Game.js
@@ -1,3 +1,7 @@
+var textArea = document.getElementById("text-area");
+textArea.innerHTML = "ello";
+
+
 function startGame(){
 	var word = "";
 	var letter = "";
@@ -6,7 +10,7 @@ function startGame(){
 	var guess = "";
 	//guess and word are set before functions or proccessing occurs.
 	var difficulty = prompt("Set Difficulty: H, M, or E");
-	console.log("Difficulty set to: " + difficulty);
+	textArea.innerHTML = ("Difficulty set to: " + difficulty);
 	setWord();
 	function setDifficulty(){
 		difficulty = difficulty.toLowerCase();
@@ -28,11 +32,11 @@ function startGame(){
 	function setWord(){
 		word = prompt("Pick a word");
 		if (word == null || undefined || !word){
-			console.log("Please insert a valid word.");
+			textArea.innerHTML = ("Please insert a valid word.");
 			setWord();
 		}
 		else{
-			console.log("Word set to: " + word);
+			textArea.innerHTML = ("Word set to: " + word);
 			console.log("Time to guess the letter in your word!");
 			guess = prompt("Guess a letter in your word");
 		}
@@ -45,24 +49,24 @@ function startGame(){
 	};
 	function checkGuess(g){
 		if (g == null || undefined || !g || g.length >= 2){
-			console.log("Please insert a valid guess.");
+			textArea.innerHTML = ("Please insert a valid guess.");
 			var guess = prompt("Guess a letter in your word");
 			checkGuess(guess);
 		}
 		else if (g != letter){
 			counter -= 1;
 			if(counter > 0){
-				console.log("Try again, " + counter + " more guesses.");
+				textArea.innerHTML = ("Try again, " + counter + " more guesses.");
 				var guess = prompt("Guess a letter in your word");
 				checkGuess(guess);
 			}
 			else{
-				console.log("Game Over")
+				textArea.innerHTML = ("Game Over")
 				console.log("Computer picked: " + letter);
 			}
 		}
 		else{
-			console.log("The correct letter is:" + " " + g + "!");
+			textArea.innerHTML = ("The correct letter is:" + " " + g + "!");
 		};
 	};
 	letterRandom(word);
