003228d2050338f3491f54a233701cb24bb96102
edwin0258 <edwin0258@gmail.com>
2015-10-04 17:56:42 -0700

    Add difficulty setting.

--- Game.js
+++ Game.js
@@ -1,14 +1,35 @@
 var word = "";
 var letter = "";
-var counter = 4;
+var counter = 0;
 //guess and word are set before functions or proccessing occurs.
+var difficulty = prompt("Set Difficulty: H, M, or E")
+console.log("Difficulty set to: " + difficulty)
 var word = prompt("Pick a word");
+console.log("Word set to: " + word)
+console.log("Time to guess the letter in your word!")
 var guess = prompt("Guess a letter in your word");
+function setDifficulty(){
+	difficulty = difficulty.toLowerCase();
+	if(difficulty == "e"){
+		counter += 7;
+	}
+	else if(difficulty == "m"){
+		counter += 4;
+	}
+	else if(difficulty == "h"){
+		counter += 2;
+	}
+	else{
+		difficulty = "m";
+		//call function again with set difficulty
+		setDifficulty();
+	}
+}
 
 function letterRandom(w){
 	var indexRandom = Math.floor(Math.random() * w.length);
 	letter = w[indexRandom];
-	console.log(letter);
+	setDifficulty()
 	checkGuess(guess);
 }
 function checkGuess(g){
@@ -26,11 +47,12 @@ function checkGuess(g){
 		}
 		else{
 			console.log("Game Over")
+			console.log("Computer picked: " + letter);
 		}
 	}
 	else{
-		console.log("The corrent letter is:" + " " + g);
+		console.log("The correct letter is:" + " " + g + "!");
 	}
 }
 letterRandom(word);
-console.log(word);
+
