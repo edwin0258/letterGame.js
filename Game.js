83f6227fe3b997f03adad345d199a218fa87093c
edwin0258 <edwin0258@gmail.com>
2015-10-04 17:23:54 -0700

    Create check guess function

--- Game.js
+++ Game.js
@@ -1,10 +1,24 @@
-var word = []
-var word = prompt("Pick a word")
-console.log(word)
+var word = [];
+//guess and word are set before functions or proccessing occurs.
+var word = prompt("Pick a word");
+var guess = prompt("Guess a letter in your word");
+console.log(word);
 var letter = "" 
 function letterRandom(w){
-	var indexRandom = Math.floor(Math.random() * w.length)
-	letter = w[indexRandom]
-	console.log(letter)
+	var indexRandom = Math.floor(Math.random() * w.length);
+	letter = w[indexRandom];
+	console.log(letter);
+	checkGuess(guess);
 }
-letterRandom(word)
+function checkGuess(g){
+	if (g == null || undefined || !g || g.length >= 2){
+		console.log("Please insert a valid guess.");
+	}
+	else if (g != letter){
+		console.log("false");
+	}
+	else{
+		console.log("true");
+	}
+}
+letterRandom(word);
